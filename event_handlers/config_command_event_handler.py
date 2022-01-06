d087b4e5c7a3c0a75076e96016c45c161579563f
Igor Pidik <igorPidik@gmail.com>
2022-01-06 21:37:51 +0100

    update the config values

--- event_handlers/config_command_event_handler.py
+++ event_handlers/config_command_event_handler.py
@@ -1,4 +1,5 @@
 from dataclasses import asdict
+from distutils.util import strtobool
 from queue import Queue
 from threading import Thread
 
@@ -38,4 +39,22 @@ class ConfigCommandEventHandler(Thread):
         event.output_provider.output(message)
 
     def __handle_set_action(self, event: ConfigCommandEvent):
-        pass
+        if len(event.args) != 2:
+            event.output_provider.output('Invalid input format')
+            return
+
+        field_type = self.config.__annotations__.get(event.args[0])
+        if not field_type:
+            event.output_provider.output(f'"{event.args[0]}" is not a valid config setting')
+            return
+
+        #  convert common string representation to boolean
+        if field_type == bool:
+            field_type = lambda x: bool(strtobool(x))
+
+        try:
+            updated_value = field_type(event.args[1])
+        except ValueError:
+            event.output_provider.output(f'"{event.args[1]}" is not a valid value for this setting')
+        else:
+            setattr(self.config, event.args[0], updated_value)
